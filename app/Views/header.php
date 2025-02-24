e8fbb1ecfd6e48b4b669993b81e34551003e5b0a
hamintonjair <hamintonjair@gmail.com>
2025-02-24 16:42:37 -0500

    correpciones

--- app/Views/header.php
+++ app/Views/header.php
@@ -30,7 +30,7 @@
                         <a class="nav-link" href="<?= base_url('/') ?>">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('videos') ?>">Videos</a>
+                        <a class="nav-link" href="<?= base_url('videos') ?>">Proyectos</a>
                     </li>
 
                 </ul>
